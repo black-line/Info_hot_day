da503bd68b1a3e1424e5aee2282bc5643ab8df0f
BlackLine <jessywuzq@gmail.com>
2016-04-11 22:20:40 +0800

    to be continue..

--- Info_hot_day.py
+++ Info_hot_day.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # coding: utf-8
-
+from urllib import request
 import urllib.request
 import urllib.parse
 import http.cookiejar
@@ -9,20 +9,20 @@ import datetime
 import sqlite3
 import hashlib
 import re
+import time
 
 
 
-
-def get_content():
+def get_content(rank_name = '时事',date = '2016/04/11'):
     # modifiable data
-    rank_name = '时事'
+
     rank_name_group = '资讯'
 
     #data
-    now = datetime.datetime.now()
-    yesterday = (now + datetime.timedelta(days = -2)).date()
-    start = yesterday
-    end = yesterday
+#    now = datetime.datetime.now()
+#    date = (now + datetime.timedelta(days = -2)).date()
+    start = date
+    end = date
 
     # url = "http://www.newrank.cn/public/info/hot.html?period=day"
     url_xhr = "http://www.newrank.cn/xdnphb/list/day/article"
@@ -51,7 +51,7 @@ def get_content():
     # set xyz /*! xdnphb linux-grunt-xdnphb-copyright 2016-03-22 */
     appBase = '/xdnphb'
     urlBase = appBase+'/'
-    xyz_str = urlBase + 'list/day/article?AppKey=joker&end=%s&rank_name=时事&rank_name_group=资讯&start=%s' % (end, start)
+    xyz_str = urlBase + 'list/day/article?AppKey=joker&end=%s&rank_name=%s&rank_name_group=资讯&start=%s' % (end, rank_name, start)
     xyz = hashlib.md5((xyz_str+'&nonce='+nonce).encode()).hexdigest()
 
     # deal with form data
@@ -71,6 +71,10 @@ def get_content():
     # deal with cookies
     cj = http.cookiejar.CookieJar()
     pro = urllib.request.HTTPCookieProcessor(cj)
+
+    # set proxy
+#    proxy_support = request.ProxyHandler({'http':'http://27.24.158.155:84'})
+#    opener = urllib.request.build_opener(proxy_support, pro)
     opener = urllib.request.build_opener(pro)
 
     op = opener.open(req, form_data)
@@ -78,25 +82,85 @@ def get_content():
 
     ori_content = json.loads(data)
     inner_content = ori_content['value']
-    print('正在获取 '+yesterday.__str__()+' 的数据')
+    print('正在获取 '+date.__str__()+' 的数据')
     return inner_content
 
+
+def get_date():
+    # modifiable data
+    rank_name = '时事'
+    rank_name_group = '资讯'
+
+#    url_xhr = "http://www.newrank.cn/xdnphb/list/day/article"
+    url_xhr = "http://www.newrank.cn/xdnphb/list/getDate"
+    req = urllib.request.Request(url_xhr)
+
+    # deal with headers
+    ori_headers = {
+        'Host': 'www.newrank.cn',
+        'Connection': 'keep-alive',
+        'Content-Length': '148',
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Origin': 'http://www.newrank.cn',
+        'X-Requested-With': 'XMLHttpRequest',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'DNT': '1',
+        'Referer': 'http://www.newrank.cn/public/info/hot.html?period=day',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4'
+    }
+
+
+    # set nance (0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f)9 of 16
+    nonce = '012345678'
+
+    # set xyz /*! xdnphb linux-grunt-xdnphb-copyright 2016-03-22 */
+    appBase = '/xdnphb'
+    urlBase = appBase+'/'
+    xyz_str = urlBase + 'list/getDate?AppKey=joker'
+    xyz = hashlib.md5((xyz_str+'&nonce='+nonce).encode()).hexdigest()
+
+    # deal with form data
+    form_data = urllib.parse.urlencode({
+        'nonce': nonce,
+        'xyz': xyz
+    }).encode()
+
+    # add headers to req
+    for key, value in ori_headers.items():
+        req.add_header(key, value)
+
+    # deal with cookies
+    cj = http.cookiejar.CookieJar()
+    pro = urllib.request.HTTPCookieProcessor(cj)
+    opener = urllib.request.build_opener(pro)
+
+    op = opener.open(req, form_data, timeout=2)
+    data = op.read().decode("UTF-8")  # <class 'str'>
+
+    ori_content = json.loads(data)
+    inner_content = ori_content['value']['WEIXIN_CAL_DAY'][:10]
+    return inner_content
 
 def store_to_db(content):
 
     # sql_create, sql_insert
     table_name = "Info_hot_day"
     sc = ''''''
-    si = ""
+    sii = ""
     i = 0
     sort_content = sorted(content[0].items(), key=lambda d: d[0])
     count = len(sort_content)
     while i < count:
         sc += '''%s TEXT,''' % sort_content[i][0]
-        si += "content[index]['%s']," % sort_content[i][0]
+        sii += "content[index]['%s']," % sort_content[i][0]
         i += 1
-    si = si[0:(len(si)-1)]
-    sc = sc[0:(len(sc)-1)]
+
+    si = sii[0:(len(sii)-1)].replace("content[index]['summary'],",'')
+    if si != sii:
+        count -= 1
+    sc = sc[0:(len(sc)-1)].replace('summary TEXT,','')
     s = "?,"*count
     s = s[0:(len(s)-1)]
     sc = re.sub(",id TEXT",",id TEXT PRIMARY KEY NOT NULL",sc)
@@ -128,6 +192,7 @@ def store_to_db(content):
         if uid_exist == 1:
             print("exist")
         else:
+
             cur.execute(sql_insert,tuple(eval(si)))
             commit_count += 1
     # Save (commit) the changes
@@ -145,12 +210,27 @@ def get_rownum_from_db(table_name):
     conn.close()
     return total[0]
 
+def main():
+    i = 0
+    num = 7
+    getDate = get_date()
+    getDate = datetime.datetime.strptime(getDate, "%Y-%m-%d").date()
+    while i < num:
+        L = ['时事','民生','财富','科技','创业','汽车','楼市','职场','教育','学术','政务','企业']
+        L = ['企业']
+        for rank_name in L:
+            date = (getDate + datetime.timedelta(days=-i))
 
-if __name__ == '__main__':
+            time.sleep(3)
+            content = get_content(rank_name,date)
+            print(content)
+            table_name = store_to_db(content)
+        i += 1
 
-    content = get_content()
-    print(content)
-    table_name = store_to_db(content)
     rownum = get_rownum_from_db(table_name)
     print('数据库中共有 '+str(rownum)+' 条数据')
+
+
+if __name__ == '__main__':
+    main()
 
